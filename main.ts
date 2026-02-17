5ccc6a0d006df3049e980d8afe7ca65b3549a40b
Evan-Downs420 <evan.downs@stu.ocsb.ca>
2026-02-17 16:38:47 -0500

    changed everything

--- main.ts
+++ main.ts
@@ -1,14 +1,21 @@
 /* Copyright (c) 2020 MTHS All rights reserved
- *
- * Created by: Evan
- * Created on: Feb 2026
- * This program will do basic math
+*
+* Created by: Evan
+* Created on: Feb 2026
+* This program will do basic math
 */
+
 basic.clearScreen()
 basic.pause(1000)
 
-basic.showString('A rectangle has dimensions 5 cm & 3 cm.')
-basic.showString('The perimeter would be')
-basic.showString((2*(5+3)).toString())
-basic.showString('The area would be')
-basic.showString((5*3).toString())
+basic.showString('Dimensions: 5cm x 3cm')
+
+// Perimeter calculation with a space for readability
+basic.showString('Perimeter: ')
+basic.showNumber(2 * (5 + 3))
+
+basic.pause(500)
+
+// Area calculation
+basic.showString('Area: ')
+basic.showNumber(5 * 3)
