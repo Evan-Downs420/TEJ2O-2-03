9e6678c33e581cfca60d2b5d9fa4959d249c9845
Evan-Downs420 <evan.downs@stu.ocsb.ca>
2026-02-17 13:23:42 -0500

    changed bassic pause and clear screen

--- main.ts
+++ main.ts
@@ -1,9 +1,12 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Evane
+ * Created by: Evan
  * Created on: Feb 2026
  * This program will do basic math
 */
+
+basic.clearScreen()
+basic.pause(1000)
 
 basic.showString('A rectangle has dimensions 5 cm & 3 cm.')
 basic.showString('The perimeter would be')
